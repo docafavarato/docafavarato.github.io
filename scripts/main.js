49b7aac112b3fb1a0f9be73852f3c8984fb4a48d
João <docafavarato@gmail.com>
2023-05-05 20:53:16 -0300

    created the translate functions

--- scripts/main.js
+++ scripts/main.js
@@ -37,3 +37,34 @@ async function deleteSentence(eleRef) {
 $(document).ready(async function() {
     await typeSentence("ullStack developer.", ".fullstack");
 });
+
+function translatePt() {
+    document.getElementById("home").innerText = "Início";
+    document.getElementById("about").innerText = "Sobre";
+    document.getElementById("projects").innerText = "Projetos";
+    document.getElementById("helloWorld").innerText = "Olá mundo,";
+    document.getElementById("iAmA").innerText = "Eu sou um";
+    document.getElementById("fullstack").innerText = "Desenvolvedor FullStack";
+    document.getElementById("portfolio").innerText = "bem vindo ao meu portfólio";
+    document.getElementById("aboutBig").innerText = "Sobre mim";
+    document.getElementById("moreAboutMe").innerText = "Mais sobre mim";
+    document.getElementById("aboutText").innerText = "Meu nome é João Pedro, e eu sou um desenvolvedor fullstack que procura sempre aprender mais. Eu começei a programar com 15 anos, e minha primeira linguagem foi python. Eu possuo experiência no desenvolvimento web e desktop, web scraping e ciência de dados.";
+    document.getElementById("projectsBig").innerText = "Projetos";
+    document.getElementById("projectsText").innerText = "Esses são alguns projetos que fiz. Todos eles e mais outros estão no meu github.";
+    document.getElementById("gptClone").innerText = "Um website de inteligência artificial no estilo ChatGPT feito com Flask e a API da OpenAI.";
+    document.getElementById("movieDb").innerText = "Um catálogo de filmes feito com Flask e a API do The Movie DataBase.";
+    document.getElementById("fileManager").innerText = "Um simples (porém funcional) gerenciador de arquivos feito com Python e PyQt5.";
+    document.getElementById("cashier").innerText = "Um ponto de venda de supermercado com diversas funções feito com Python e PyQt5.";
+    document.getElementById("youtubeConverter").innerText = "Um conversor de vídeos do YouTube (mp3/mp4) feito com Python, PyQt5 e PyTube.";
+    document.getElementById("flappyBird").innerText = "Remake do famoso Flappy Bird, feito em C++ com a biblioteca SFML.";
+    document.getElementById("keyboardSmasher").innerText = "Um jogo no estilo arcade feito em C++ com a biblioteca SFML. Esmague esse teclado!";
+    document.getElementById("csgoStats").innerText = "Um tracker para o Counter-Strike: Global Offensive feito com Flask e a API do tracker.gg.";
+    const links = document.getElementsByClassName("a-round");
+    for (const link of links) {
+        link.innerText = "Visitar";
+    } 
+}
+
+function translateEn() {
+    location.reload();
+}
