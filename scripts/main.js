1f0d86f3e3294833534d1e265d9b0f1ed76bd7dc
João <docafavarato@gmail.com>
2023-05-19 21:07:07 -0300

    tooltip

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 function scrollToAbout() {
     window.scrollTo(0, 500);
 }
